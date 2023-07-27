56b1ac019074483f32220df73a3bc33c4c093e38
kitkas0000 <97841577+kitkas0000@users.noreply.github.com>
2023-07-27 13:52:22 +0700

    add favorites function

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,13 +4,13 @@ import SearchInput from "@/components/SearchInput";
 
 import SearchContent from "./components/SearchContent";
 
-export const revalidate = 0;
-
 interface SearchProps {
   searchParams: {
     title: string;
   };
 }
+
+export const revalidate = 0;
 
 const Search = async ({ searchParams }: SearchProps) => {
   const songs = await getSongsByTitle(searchParams.title);
